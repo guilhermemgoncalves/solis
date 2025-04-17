c41bf4ba5b0d5bbae433bb0bf21a61779f609021
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-17 01:46:49 -0300

    feat: atualização de cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'], // Allow all origins
+    origin: ['http://localhost:4200', 'https://arete-infra-frontend-jul2.vercel.app'], // Allow all origins
   })
   await app.listen(process.env.PORT ?? 3000);
 }
